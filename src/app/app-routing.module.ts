00ccad4e6faa062d0c5c1af972d404201430a74d
Loïc TESSIER <loictessier.inge@gmail.com>
2019-03-31 15:54:56 +0200

    Routing and basic Book model

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { BookFormComponent } from './book-list/book-form/book-form.component';
+import { SingleBookComponent } from './book-list/single-book/single-book.component';
+
+const routes: Routes = [
+    { path: 'auth/signup', component: SignupComponent },
+    { path: 'auth/signin', component: SigninComponent },
+    { path: 'books', component: BookListComponent },
+    { path: 'books/new', component: BookFormComponent },
+    { path: 'books/view/:id', component: SingleBookComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
