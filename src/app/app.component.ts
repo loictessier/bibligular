15165691047365f1bd30ffa318a6e1dfe19ba160
Loïc TESSIER <loictessier.inge@gmail.com>
2019-03-31 16:36:15 +0200

    Config app firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bibligular';
+
+    constructor() {
+        // Initialize Firebase
+        const config = {
+            apiKey: 'AIzaSyDBl_7RvfEEKSs6uzKG8NfP5mk1V_ugwBc',
+            authDomain: 'bibligular.firebaseapp.com',
+            databaseURL: 'https://bibligular.firebaseio.com',
+            projectId: 'bibligular',
+            storageBucket: '',
+            messagingSenderId: '962996594869'
+        };
+        firebase.initializeApp(config);
+    }
 }
